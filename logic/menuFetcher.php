713ece930b145bb0af35f5f61580e7e3f31f0704
Andrej Zarkovski <zarke998@gmail.com>
2020-06-18 17:53:18 +0200

    Add documentation link

--- logic/menuFetcher.php
+++ logic/menuFetcher.php
@@ -8,7 +8,8 @@
         global $conn;
         $selectQuery = "SELECT menu_items.name AS item_name, href, role_id FROM menu_items
                                 INNER JOIN menus ON menus.id = menu_items.menu_id
-                                WHERE menus.name = :menu;";
+                                WHERE menus.name = :menu
+                                ORDER BY menu_items.id";
         $stm = $conn->prepare($selectQuery);
         $stm->bindParam(":menu",$menuName);
         $stm->execute();
