3b090fa903e450af52a320f0ce31ac23060c758e
Andrej <zarke998@gmail.com>
2021-08-10 20:52:21 +0200

    feat: remove test db credentials

--- logic/dbConnection.php
+++ logic/dbConnection.php
@@ -1,25 +1,14 @@
 <?php
 
-    
-    $debug = true;
-
     $host = "localhost";
-    if($debug){
-        $dbName = "hometheater";
-        $dbUser = "root";
-        $dbPassword = "";
-    }
-    else{
-        $dbName = "id12855867_hometheater";
-        $dbUser = "id12855867_zarke998";
-        $dbPassword = "qawsedrft117";
-    }
-
+    $dbName = "hometheater";
+    $dbUser = "root";
+    $dbPassword = "";
+    
     try{
         $conn = new PDO("mysql:host=$host;dbname=$dbName",$dbUser,$dbPassword);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-
     }
     catch(PDOException $e){
         http_response_code(500);
